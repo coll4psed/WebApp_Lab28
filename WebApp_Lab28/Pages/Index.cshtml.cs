0d88797d1a04c6497ac9cfce0ae314fe4f8a65e3
coll4psed <dmitry.orlov00@yandex.ru>
2024-03-21 22:02:18 +0300

    Добавьте файлы проекта.

---
+++ WebApp_Lab28/Pages/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Diagnostics;
+
+namespace WebApp_Lab28.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost(string userName, string password)
+        {
+            MySqlConnection conn = new MySqlConnection("server=sutct.org;user=1_08;database=1_08;port=3306;password=8HIzHoFkAh6");
+            conn.Open();
+            try
+            {
+                MySqlCommand cmd = conn.CreateCommand();
+                string sql = "SELECT * FROM user_info WHERE Nickname=" + userName;
+                cmd.CommandText = sql;
+                cmd.Connection = conn;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            conn.Close();
+        }
+    }
+}
